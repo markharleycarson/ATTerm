cfd9427dae20c8975a45350dc10a0aabb3c55dda
Mark Carson <mcarson@ipi.co.za>
2020-05-31 12:05:53 +0200

    Add about box and other fixes

---
+++ ATTerm/frmAbout.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ATTerm
+{
+    partial class frmAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtAbout = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(354, 257);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAbout
+            // 
+            this.txtAbout.Location = new System.Drawing.Point(12, 12);
+            this.txtAbout.Multiline = true;
+            this.txtAbout.Name = "txtAbout";
+            this.txtAbout.Size = new System.Drawing.Size(453, 221);
+            this.txtAbout.TabIndex = 1;
+            // 
+            // frmAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 309);
+            this.Controls.Add(this.txtAbout);
+            this.Controls.Add(this.btnClose);
+            this.Name = "frmAbout";
+            this.Text = "About ATTerm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtAbout;
+    }
+}
---
+++ ATTerm/frmAbout.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATTerm
+{
+    public partial class frmAbout : Form
+    {
+        string strVersion = "";
+        public frmAbout()
+        {
+            InitializeComponent();
+            strVersion = Application.ProductVersion;
+
+            txtAbout.Text = "          AT GSM Test Application        " + Environment.NewLine +
+                "             version: 1.0.0.10         " + Environment.NewLine +
+                "          MH Carson       " + Environment.NewLine +
+                "" + Environment.NewLine +
+                " Donations and Assistance: https://ko-fi.com/markcarson" + Environment.NewLine +
+                " https://github.com/markharleycarson/ATTerm " + Environment.NewLine +
+                
+                "Application Version" + strVersion +
+                "" +
+                "  (based on original idea https://github.com/oakkar7 and ATCommand?) ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
